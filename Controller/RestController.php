b32d68d3a1fd2306b114862b34b0e0096cf7ad5f
dantleech <dan.t.leech@gmail.com>
2015-01-27 11:37:03 +0000

    Added controllerd

---
+++ Controller/RestController.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of the Massive CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Massive\Bundle\SearchBundle\Controller;
+
+use Massive\Bundle\SearchBundle\Search\SearchManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class RestController
+{
+    /**
+     * @var SearchManagerInterface
+     */
+    private $searchManager;
+
+    /**
+     * @param SearchManagerInterface $searchManager
+     */
+    public function __construct(SearchManagerInterface $searchManager)
+    {
+        $this->searchManager = $searchManager;
+    }
+
+    /**
+     * Perform a search and return a JSON response
+     *
+     * @param mixed $query Search query
+     * @param array $indexes (optional) list of indexes
+     * @param mixed $locale (optional) locale to search in
+     */
+    public function searchAction($query, $indexes = array(), $locale = null)
+    {
+        $hits = $this->searchManager
+            ->createSearch($query)
+            ->locale($locale)
+            ->indexes($indexes)
+            ->execute();
+
+        return new JsonResponse($hits);
+    }
+}
