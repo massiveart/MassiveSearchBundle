32f2c01cfb1e45f4b73593f51c161b8a47c8f504
dantleech <dan.t.leech@gmail.com>
2015-02-03 09:36:33 +0000

    Added Massive-ExpressionLanguage

--- Search/EventSubscriber/DoctrineOrmSubscriber.php
+++ Search/EventSubscriber/DoctrineOrmSubscriber.php
@@ -88,5 +88,4 @@ class DoctrineOrmSubscriber implements EventSubscriber
             return;
         }
     }
-
 }
---
+++ Search/ExpressionLanguage/MassiveSearchExpressionLanguage.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Massive\Bundle\SearchBundle\Search\ExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+
+/**
+ * Expression language for massive search bundle
+ */
+class MassiveSearchExpressionLanguage extends ExpressionLanguage
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function registerFunctions()
+    {
+        parent::registerFunctions();
+
+        $this->addFunction($this->createJoinFunction());
+        $this->addFunction($this->createMapFunction());
+    }
+
+    /**
+     * Join is an alias for PHP implode:
+     *
+     *   join(',', ['one', 'two', 'three']) = "one,two,three"
+     *
+     * @return ExpressionFunction
+     */
+    private function createJoinFunction()
+    {
+        return new ExpressionFunction(
+            'join',
+            function ($glue, $elements) {
+                return sprintf('join(%s, %s)', $glue, $elements);
+            },
+            function (array $values, $glue, $elements) {
+                return join($glue, $elements);
+            }
+        );
+    }
+
+    /**
+     * Join is an analogue for array_map. The callback
+     * in the form of an expression. The nested expression has
+     * one variable, "el".
+     *
+     * For example:
+     *
+     *   map({'foo': 'one', 'foo': 'two'}, 'el["foo"]'}) = array('one', 'two');
+     *
+     * @return ExpressionFunction
+     */
+    private function createMapFunction()
+    {
+        return new ExpressionFunction(
+            'map',
+            function ($elements, $expression) {
+                throw new \Exception('Map function does not support compilation');
+            },
+            function (array $values, $elements, $expression) {
+                $expressionLanguage = $this;
+                return array_map(function ($element) use ($expressionLanguage, $expression) {
+                    return $expressionLanguage->evaluate($expression, array(
+                        'el' => $element
+                    ));
+                }, $elements);
+            }
+        );
+    }
+}
