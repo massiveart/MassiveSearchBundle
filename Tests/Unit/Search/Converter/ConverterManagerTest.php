90de6f924c1f7090dd151132969dab014faadcec
Luca Rath-Heel <luca@sulu.io>
2021-02-15 17:51:20 +0100

    Fix tests

--- Tests/Unit/Search/Converter/ConverterManagerTest.php
+++ Tests/Unit/Search/Converter/ConverterManagerTest.php
@@ -14,6 +14,7 @@ namespace Tests\Unit\Search\Converter;
 use Massive\Bundle\SearchBundle\Search\Converter\ConverterInterface;
 use Massive\Bundle\SearchBundle\Search\Converter\ConverterManager;
 use Massive\Bundle\SearchBundle\Search\Converter\ConverterNotFoundException;
+use Massive\Bundle\SearchBundle\Search\Document;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 
@@ -21,8 +22,23 @@ class ConverterManagerTest extends TestCase
 {
     public function testConvert()
     {
+        $document = $this->prophesize(Document::class);
         $converter = $this->prophesize(ConverterInterface::class);
-        $converter->convert([1, 2, 3])->willReturn('1,2,3');
+        $converter->convert([1, 2, 3], $document->reveal())->willReturn('1,2,3');
+
+        $converterManager = new ConverterManager();
+        $converterManager->addConverter('tags', $converter->reveal());
+
+        $result = $converterManager->convert([1, 2, 3], 'tags', $document->reveal());
+
+        $this->assertEquals('1,2,3', $result);
+        $converter->convert([1, 2, 3], $document->reveal())->shouldBeCalled();
+    }
+
+    public function testConvertWithoutDocument()
+    {
+        $converter = $this->prophesize(ConverterInterface::class);
+        $converter->convert([1, 2, 3], null)->willReturn('1,2,3');
 
         $converterManager = new ConverterManager();
         $converterManager->addConverter('tags', $converter->reveal());
@@ -30,7 +46,7 @@ class ConverterManagerTest extends TestCase
         $result = $converterManager->convert([1, 2, 3], 'tags');
 
         $this->assertEquals('1,2,3', $result);
-        $converter->convert([1, 2, 3])->shouldBeCalled();
+        $converter->convert([1, 2, 3], null)->shouldBeCalled();
     }
 
     public function testConverterNotExists()
